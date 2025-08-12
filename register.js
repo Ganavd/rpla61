05ccb96df4e7a33e53a554b45c1b8c2c1ef4a583
Ganavd <bagussitee@gmail.com>
2025-08-12 09:07:50 +0700

    Update register.js

--- register.js
+++ register.js
@@ -1,15 +1,13 @@
-
-
-document.getElementById('formRegister').addEventListener('submit', e => {
+document.getElementById('formReg').addEventListener('submit', e => {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(e.target));
   const kode = data.kode.toUpperCase();
   if (!cekKode(kode)) {
-    alert('Kode tidak valid atau sudah dipakai');
+    alert('Kode tidak valid / sudah dipakai');
     return;
   }
   tandaiKode(kode);
   localStorage.setItem(data.user, JSON.stringify(data));
   alert('Akun berhasil dibuat!');
-  closeModalBox();
+  location.href = 'login.html';
 });
